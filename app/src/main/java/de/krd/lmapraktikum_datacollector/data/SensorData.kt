66c4c8700b1663f20e2fee441f77ffe698e9f3a9
keredra <none>
2020-11-26 11:27:47 +0100

    fix sensor timestamp

--- app/src/main/java/de/krd/lmapraktikum_datacollector/data/SensorData.kt
+++ app/src/main/java/de/krd/lmapraktikum_datacollector/data/SensorData.kt
@@ -1,6 +1,8 @@
 package de.krd.lmapraktikum_datacollector.data
 
 import android.hardware.SensorEvent
+import android.os.SystemClock
+
 
 data class SensorData(
     val timestamp: Long,
@@ -9,9 +11,11 @@ data class SensorData(
     val values: FloatArray
 ) {
     companion object {
+        private val startUpTime = System.currentTimeMillis()
+
         fun fromSensorEvent(sensorEvent: SensorEvent) : SensorData {
             return SensorData(
-                sensorEvent.timestamp,
+                startUpTime + sensorEvent.timestamp / 1000000,
                 sensorEvent.sensor.type,
                 sensorEvent.sensor.name,
                 sensorEvent.values
