cfb334aaa6a86eee3455c07dc41fb69aa96beeef
keredra <none>
2020-11-26 11:28:09 +0100

    fix home screen

--- app/src/main/java/de/krd/lmapraktikum_datacollector/ui/home/HomeFragment.kt
+++ app/src/main/java/de/krd/lmapraktikum_datacollector/ui/home/HomeFragment.kt
@@ -1,8 +1,5 @@
 package de.krd.lmapraktikum_datacollector.ui.home
 
-import android.annotation.SuppressLint
-import android.content.Context
-import android.content.Context.MODE_PRIVATE
 import android.content.SharedPreferences
 import android.os.Bundle
 import android.util.Log
@@ -10,7 +7,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.SeekBar
-import android.widget.ArrayAdapter
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.Observer
@@ -19,16 +15,18 @@ import de.krd.lmapraktikum_datacollector.GlobalModel
 import de.krd.lmapraktikum_datacollector.R
 import de.krd.lmapraktikum_datacollector.data.LocationData
 import de.krd.lmapraktikum_datacollector.data.SensorData
-import de.krd.lmapraktikum_datacollector.ui.location_data.LocationListviewAdapter
-import de.krd.lmapraktikum_datacollector.ui.sensor_data.SensorListviewAdapter
+import de.krd.lmapraktikum_datacollector.utils.PreferenceHelper
 import kotlinx.android.synthetic.main.fragment_home.*
 import java.util.*
-import kotlin.collections.ArrayList
 
 class HomeFragment : Fragment(), Observer<MutableList<SensorData>>  {
     private val model: GlobalModel by activityViewModels()
-    private lateinit var arrayAdapter: ArrayAdapter<SensorData>
     private lateinit var preferences: SharedPreferences
+    private lateinit var preferencesEditor: SharedPreferences.Editor
+
+    private lateinit var sensorObserver: Observer<MutableList<SensorData>>
+    private lateinit var locationObserver: Observer<MutableList<LocationData>>
+
     override fun onCreateView(
             inflater: LayoutInflater,
             container: ViewGroup?,
@@ -36,22 +34,25 @@ class HomeFragment : Fragment(), Observer<MutableList<SensorData>>  {
     ): View? {
         return inflater.inflate(R.layout.fragment_home, container, false)
     }
-    @SuppressLint("NewApi")
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
 
-        val seekBar = seekBar
-        seekBar.min=10000
-        seekBar.max=5000000
-        preferences = getDefaultSharedPreferences(context)
+        //val seekBar = seekBar ?????
+        val rActivity = requireActivity()
+        preferences = getDefaultSharedPreferences(rActivity)
+        preferencesEditor = preferences.edit()
+        val periodId = rActivity.getString(R.string.setting_sensor_sampling_period);
+
+        seekBar.progress = PreferenceHelper.getInt(rActivity, preferences, R.string.setting_sensor_sampling_period)
         seekBar.setOnSeekBarChangeListener(object: SeekBar.OnSeekBarChangeListener{
             override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
-                val samplingRate = progress.toLong()
-
-                val preferencesEditor = preferences.edit()
-                Log.d("Progress", samplingRate.toString())
-                preferencesEditor.putLong("@string/setting_sensor_sampling_period",samplingRate)
+                val value = ""+progress
+                //Log.d("Progress", samplingRate.toString()) // ?????
+                Log.d("Progress", value)
+                //preferencesEditor.putLong("@string/setting_sensor_sampling_period",samplingRate) ???
+                preferencesEditor.putString(periodId, value)
                 preferencesEditor.commit()
             }
 
@@ -62,37 +63,49 @@ class HomeFragment : Fragment(), Observer<MutableList<SensorData>>  {
             }
 
         })
-        model.data.sensorEvents.observe(viewLifecycleOwner, Observer {
-            val adapter = SensorListviewAdapter(requireActivity(), model.data.sensorEvents.value.filter {it.type == 1 } as ArrayList<SensorData>)
-            if(adapter.getCount()>1) {
-                val currentAccelerometer = adapter.getItem(adapter.getCount()-1)
-                val lastAccelerometer = adapter.getItem(adapter.getCount()-2)
-                tv_currAcc.text = "Aktuelle Accelerometerwerte (X, Y, Z, Timestamp):\n" +currentAccelerometer.values[0].toString()+", "+currentAccelerometer.values[1].toString()+", "+currentAccelerometer.values[2].toString()+", "+ Date(currentAccelerometer.timestamp).toString()
-                tv_lastAcc.text = "Letzte Accelerometerwerte (X, Y, Z, Timestamp):\n" +lastAccelerometer.values[0].toString()+", "+lastAccelerometer.values[1].toString()+", "+lastAccelerometer.values[2].toString()+", "+Date(lastAccelerometer.timestamp).toString()
-                tv_currSampRateAcc.text = "Aktuelle Abtastrate Accelerometer: " +(currentAccelerometer.timestamp-lastAccelerometer.timestamp).toString()
+
+        sensorObserver = Observer<MutableList<SensorData>> { sensorDataList ->
+            val accelerometerDataList = sensorDataList.filter { it.type == 1 }
+            val gyroscopeDataList = sensorDataList.filter { it.type == 4 }
+            if(accelerometerDataList.size > 1) {
+                val currentAccelerometer = accelerometerDataList.get(accelerometerDataList.size-1)
+                val lastAccelerometer = accelerometerDataList.get(accelerometerDataList.size-2)
+                tv_currAcc.text = "Aktuelle Accelerometerwerte (X, Y, Z, Timestamp):\n" +currentAccelerometer.values[0]+", "+currentAccelerometer.values[1]+", "+currentAccelerometer.values[2]+", "+ Date(currentAccelerometer.timestamp)
+                tv_lastAcc.text = "Letzte Accelerometerwerte (X, Y, Z, Timestamp):\n" +lastAccelerometer.values[0]+", "+lastAccelerometer.values[1]+", "+lastAccelerometer.values[2]+", "+Date(lastAccelerometer.timestamp)
+                tv_currSampRateAcc.text = "Aktuelle Abtastrate Accelerometer: " +(currentAccelerometer.timestamp-lastAccelerometer.timestamp)
             }
-            val adapterGyro = SensorListviewAdapter(requireActivity(), model.data.sensorEvents.value.filter { it.type == 4 } as ArrayList<SensorData>)
-            if (adapterGyro.getCount() > 1) {
-                val currentGyroskop = adapterGyro.getItem(adapterGyro.getCount() - 1)
-                val lastGyroskop = adapterGyro.getItem(adapterGyro.getCount() - 2)
-                tv_currGyro.text = "Aktuelle Gyroskopwerte (X, Y, Z, Timestamp):\n" + currentGyroskop.values[0].toString() + ", " + currentGyroskop.values[1].toString() + ", " + currentGyroskop.values[2].toString() + ", " + Date(currentGyroskop.timestamp).toString()
-                tv_lastGyro.text = "Letzte Gyroskopwerte (X, Y, Z, Timestamp):\n" + lastGyroskop.values[0].toString() + ", " + lastGyroskop.values[1].toString() + ", " + lastGyroskop.values[2].toString() + ", " + Date(lastGyroskop.timestamp).toString()
-                tv_currSampRateGyro.text = "Aktuelle Abtastrate Gyroskop: " + (currentGyroskop.timestamp - lastGyroskop.timestamp).toString()
+            if (gyroscopeDataList.size > 1) {
+                val currentGyroskop = gyroscopeDataList.get(gyroscopeDataList.size - 1)
+                val lastGyroskop = gyroscopeDataList.get(gyroscopeDataList.size - 2)
+                tv_currGyro.text = "Aktuelle Gyroskopwerte (X, Y, Z, Timestamp):\n" + currentGyroskop.values[0] + ", " + currentGyroskop.values[1] + ", " + currentGyroskop.values[2] + ", " + Date(currentGyroskop.timestamp)
+                tv_lastGyro.text = "Letzte Gyroskopwerte (X, Y, Z, Timestamp):\n" + lastGyroskop.values[0] + ", " + lastGyroskop.values[1] + ", " + lastGyroskop.values[2] + ", " + Date(lastGyroskop.timestamp)
+                tv_currSampRateGyro.text = "Aktuelle Abtastrate Gyroskop: " + (currentGyroskop.timestamp - lastGyroskop.timestamp)
             }
-        })
-        model.data.locations.observe(viewLifecycleOwner, Observer {
-            val adapterLocation = LocationListviewAdapter(requireActivity(), model.data.locations.value as ArrayList<LocationData>)
-            if(adapterLocation.getCount()>1) {
-                val currentLocation = adapterLocation.getItem(adapterLocation.getCount() - 1)
-                val lastLocation = adapterLocation.getItem(adapterLocation.getCount() - 2)
-                tv_currLocation.text = "Aktuelle Positionsdaten (Latitude, Longitude, Altitude, Timestamp):\n" + currentLocation.latitude.toString() + ", " + currentLocation.longitude.toString() + ", " + currentLocation.altitude.toString() + ", " + Date(currentLocation.timestamp).toString()
-                tv_lastLocation.text = "Letzte Positionsdaten (Latitude, Longitude, Altitude, Timestamp):\n" + lastLocation.latitude.toString() + ", " + lastLocation.longitude.toString() + ", " + lastLocation.altitude.toString() + ", " + Date(lastLocation.timestamp).toString()
-                tv_currSampRateLocation.text = "Aktuelle Abtastrate Position: " + (currentLocation.timestamp - lastLocation.timestamp).toString()
+        }
+        locationObserver = Observer { locationDataList ->
+            if(locationDataList.size > 1) {
+                val currentLocation = locationDataList.get(locationDataList.size - 1)
+                val lastLocation = locationDataList.get(locationDataList.size - 2)
+                tv_currLocation.text = "Aktuelle Positionsdaten (Latitude, Longitude, Altitude, Timestamp):\n" + currentLocation.latitude + ", " + currentLocation.longitude + ", " + currentLocation.altitude + ", " + Date(currentLocation.timestamp)
+                tv_lastLocation.text = "Letzte Positionsdaten (Latitude, Longitude, Altitude, Timestamp):\n" + lastLocation.latitude + ", " + lastLocation.longitude + ", " + lastLocation.altitude + ", " + Date(lastLocation.timestamp)
+                tv_currSampRateLocation.text = "Aktuelle Abtastrate Position: " + (currentLocation.timestamp - lastLocation.timestamp)
             }
-        })
+        }
+
     }
 
+    override fun onPause() {
+        super.onPause()
+        model.data.sensorEvents.removeObserver(sensorObserver)
+        model.data.locations.removeObserver(locationObserver)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        model.data.sensorEvents.observe(viewLifecycleOwner, sensorObserver)
+        model.data.locations.observe(viewLifecycleOwner, locationObserver)
+    }
     override fun onChanged(t: MutableList<SensorData>?) {
-        arrayAdapter.notifyDataSetChanged()
+
     }
 }
